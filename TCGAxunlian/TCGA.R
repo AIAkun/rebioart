c08f070f11e2238f4c785417d8ce65852c1b16bb
ak <13033853935@163.com>
2024-02-29 18:32:47 +0800

    .

--- TCGAxunlian/TCGA.R
+++ TCGAxunlian/TCGA.R
@@ -210,3 +210,44 @@ ESTIMATE_result<-read.table("ESTIMATE_result.txt",sep="\t",row.names=1,check.nam
 identical(rownames(ESTIMATE_result),rownames(surv_01A))
 ESTIMATE_result_surv_01A<-cbind(surv_01A,ESTIMATE_result)
 write.table(ESTIMATE_result_surv_01A,"ESTIMATE_result_surv_01A.txt",sep="\t",row.names=T,col.names=NA,quote=F)
+
+
+
+# 根据ESTIMATE_result高低组做生存分析
+surv <- ESTIMATE_result_surv_01A
+surv$OS.time <- surv$OS.time/365
+#根据median中位数分组
+# ImmuneScore
+surv$group <- ifelse(surv$ImmuneScore>median(surv$ImmuneScore),'High','Low')
+surv$group <- factor(surv$group,levels = c('Low','High'))
+class(surv$group)
+table(surv$group)
+library(survival)
+fitd <- survdiff(Surv(OS.time,OS) ~ group,data = surv,na.action = na.exclude)
+pValue <- 1-pchisq(fitd$chisq,length(fitd$n)-1)
+# 拟合生存曲线
+fit <- survfit(Surv(OS.time,OS) ~ group,data = surv)
+summary(fit)
+p.lab <- paste0('P',ifelse(pValue<0.001," <0.001",paste0(" = ",round(pValue,3))))
+library(survminer)
+ggsurvplot(fit,data = surv,
+           pval = p.lab,
+           conf.int = TRUE,#显示置信区间
+           risk.table = TRUE,#显示风险表
+           risk.table.col="strata",
+           palette='jco',#配色
+           legend.labs=c("Low","High"),#图例
+           size=1,
+           xlim=c(0,20),#x轴长度
+           break.time.by=5,#x轴步长为5
+           legend.title='ImmuneScore',
+           surv.median.line = 'hv',#限制垂直和水平
+           ylab='Survival probability(%)',#修改y轴标签
+           xlab='Time(years)',#修改x轴标签
+           ncensor.plot=TRUE,#显示删失图块
+           ncensor.plot.height=0.25,
+           risk.table.y.text=FALSE
+           )
+dev.off()
+
+
